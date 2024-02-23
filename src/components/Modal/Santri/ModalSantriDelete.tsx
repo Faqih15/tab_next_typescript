1303f8a29119dc8ae31848894939dd5de9fd189b
mujahidfaqih <mujahidfaqihuddin@gmail.com>
2024-02-23 09:35:30 +0700

    feat(santriDelete): delete santri by id fetch

--- src/components/Modal/Santri/ModalSantriDelete.tsx
+++ src/components/Modal/Santri/ModalSantriDelete.tsx
@@ -13,8 +13,57 @@ type KeySantri = {
   no_tabungan: string;
 };
 
+// async function deleteById(id) {
+//   try {
+//     const response = await fetch(`http://localhost:5000/santri/${id}`, {
+//       method: 'DELETE',
+//       headers: {
+//         'Content-Type': 'application/json'
+//         // You may need to include additional headers like authorization token if required
+//       }
+//     });
+
+//     if (!response.ok) {
+//       throw new Error('Failed to delete data');
+//     }
+
+//     const data = await response.json();
+//     return data; // Returning any response data if needed
+//   } catch (error) {
+//     console.error('Error deleting data:', error);
+//     throw error;
+//   }
+// }
+
 export default function DeleteSantri({paramDelete, onClose} :any) {
-  console.log('ModalTopUp item', paramDelete)
+  // console.log('ModalDelete item', paramDelete)
+  // const id = paramDelete.id;
+  const handleDelete = async () => {
+    const id1 = paramDelete.id;
+    try {
+    const id2 = paramDelete.id;
+      try {
+        const response = await fetch(`http://localhost:5000/santri/${id2}`, {
+          method: 'DELETE',
+        });
+        console.log('id2', id2, 'id1', id1)
+    
+        if (!response.ok) {
+          throw new Error('Failed to delete data line 53');
+        }
+    
+        // const data = await response.json();
+        // return data; // Returning any response data if needed
+      } catch (error) {
+        console.error('Error deleting data line 59 :', error);
+        throw error;
+      }
+      onClose(); // Close modal or perform any other action after deletion
+    } catch (error) {
+      console.error('Failed to delete data line 64 :', error);
+    }
+  };
+
   return (
     <section>
       <div
@@ -42,75 +91,25 @@ export default function DeleteSantri({paramDelete, onClose} :any) {
               </div>
               {/* <!-- Modal body --> */}
               {!!paramDelete ? (
-                // <form>
-                //   <div className="max-w-sm mx-auto py-5">
-                //     <div className="mb-3">
-                //       <label className="block text-base font-medium text-gray-900">
-                //         Nama Santri
-                //       </label>
-                //       <input
-                //         type="text"
-                //         id="username"
-                //         defaultValue={item?.nama}
-                //         className="shadow-sm bg-gray-50 border border-gray-400 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
-                //         placeholder="nama"
-                //         required
-                //         disabled
-                //         readOnly
-                //       />
-                //     </div>
-                //     <div className="mb-3">
-                //       <label className="block text-base font-medium text-gray-900">
-                //         No Tabungan
-                //       </label>
-                //       <input
-                //         type="text"
-                //         id="no_tabungan"
-                //         defaultValue={item?.no_tabungan}
-                //         className="shadow-sm bg-gray-50 border border-gray-400 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
-                //         placeholder="nim"
-                //         required
-                //         disabled
-                //         readOnly
-                //       />
-                //     </div>
-                //     <div className="mb-3">
-                //       <label className="block text-base font-medium text-gray-900">
-                //         NIM
-                //       </label>
-                //       <input
-                //         type="number"
-                //         id="nim"
-                //         defaultValue={item?.nim}
-                //         className="shadow-sm bg-gray-50 border border-gray-400 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
-                //         placeholder="nim"
-                //         required
-                //         disabled
-                //         readOnly
-                //       />
-                //     </div>
-                //     <div className="mb-3">
-                //       <label className="block text-base font-medium text-gray-900">
-                //         Uang masuk
-                //       </label>
-                //       <input
-                //         type="number"
-                //         id="saldoin"
-                //         defaultValue="0"
-                //         className="shadow-sm bg-gray-50 border border-blue-400 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
-                //         placeholder="nim"
-                //         required
-                //       />
-                //     </div>
-                //     <button
-                //       type="submit"
-                //       className="bg-blue-600 hover:bg-blue-700  text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                //     >
-                //       Submit Nabung
-                //     </button>
-                //   </div>
-                // </form>
-                <div className="text-2xl text-orange-600 text-bold py-5 px-4 text-center"> are you sure to delete it? </div>
+                <section className="flex flex-col justify-center items-center py-8 gap-3"> 
+                  <span className="text-4xl text-red-600 font-extrabold capitalize py-5 px-4 text-center mb-3">are you sure to delete it?</span>   
+                  <div className="px-5 flex justify-center items-center gap-2">
+                    <button
+                      type="button"
+                      className="bg-red-500 hover:bg-red-600 text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 text-center"
+                      onClick={handleDelete}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      type="button"
+                      className="bg-gray-100 border border-gray-400 hover:bg-gray-400 text-black focus:outline-none focus:ring-4 focus:ring-gray-200  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                      onClick={onClose}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </section>
               ) : (
                 <div className="text-2xl text-red-600 text-bold py-5 px-4 text-center"> error when sending `paramDelete` as props </div>
               )}
